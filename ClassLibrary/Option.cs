9b0e27a89b0f1aaee6e25de7b33077a9c4d7eac8
renan rosa <renan.throsa@gmail.com>
2026-06-10 13:53:49 -0300

    Implicity convertiond added.

--- ClassLibrary/Option.cs
+++ ClassLibrary/Option.cs
@@ -1,8 +1,38 @@
-﻿namespace ClassLibrary
-{
-    public interface Option<T> { }
-    public record None<T> : Option<T>;
-    public record Some<T>(T Value) : Option<T>;
+﻿using System;
 
+namespace ClassLibrary
+{
+    public struct NoneType { }
+    public record None<T> : Option<T> { }
+    public abstract record Option<T>
+    {
+        /// <summary>
+        /// This effectively tells the runtime that an instance of NoneType can be used where an
+        /// Option<T>  is  expected and  instructs the  runtime to  convert the  NoneType to a
+        /// None<T>.
+        /// </summary>
+        /// <param name="_"></param>
+        public static implicit operator Option<T>(NoneType _)
+            => new None<T>();
+
+        /// <summary>
+        /// Implicit conversion from T to Option<T>. This means that a T can be used where an Option<T> is expected 
+        /// and will automatically be wrapped into a Some<T>—unless it’s null, in which case it will be a None<T>.
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator Option<T>(T value)
+            => value is null ? new None<T>() : new Some<T>(value);
+
+    }
+
+    public record Some<T> : Option<T>
+    {
+        private T Value { get; }
+
+        public Some(T value) => Value = value ?? throw new ArgumentNullException();
+
+        public void Deconstruct(out T value) => value = Value;
+
+    }
 
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,17 +1,29 @@
 ﻿using ClassLibrary;
+using System.Collections.Specialized;
+
 
 namespace ConsoleApp
 {
     internal class Program
     {
+        public static readonly NoneType None = default;
+
         static string Greet(Option<string> greetee)
             => greetee.Match(None: () => "Sorry, who?", Some: (name) => $"Hello, {name}");
+
+        public static Option<T> Some<T>(T t) => new Some<T>(t);
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Greet(new None<string>()));
-            Console.WriteLine(Greet(new Some<string>("John")));
+            Console.WriteLine(Greet(None));
+            Console.WriteLine(Greet("John"));
+
+            var empty = new NameValueCollection();
+            Option<string> green = empty["green"];
+            Console.WriteLine(green);  // => None
+
         }
+
     }
 }
